6820439bac90cc0ee3c0c7afb725a2970ebeb4c4
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2023-08-01 10:52:31 -0500

    Inicializacion de proyecto

---
+++ src/main/java/com/mycompany/sistemabuses/SistemaBuses.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemabuses;
+
+/**
+ *
+ * @author Richely
+ */
+public class SistemaBuses {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
